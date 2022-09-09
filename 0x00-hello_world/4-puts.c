a8306d29673e6720c8e6506d07b690c23b8465a4
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-08 19:25:57 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/*
+ * main where code is always run from
+ * return 0 is proving the code run was successful
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual language");
+}
