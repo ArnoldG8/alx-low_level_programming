6aed90aa74700fb0ff5b84b376e6d3fcf8d70da8
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-15 15:42:23 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,28 @@ int print_sign(int c)
 	if (c > 0)
 	{
 		_putchar(43);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(1 + '0');
+		_putchar('\n');
 		return (1);
 	}
 	else if (c == 0)
 	{
 		_putchar(48);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(0 + '0');
+		_putchar('\n');
 		return (0);
 	}
 	else
 	{
 		_putchar(45);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(-1 + '0);
+		_putchar('\n');
 		return (-1);
 	}
 	_putchar('\n');
