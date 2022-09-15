3720d3cf1acd3f8faf00d12961d8933bc38f91b8
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-15 14:40:55 -0700

    taskTwo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main  Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
