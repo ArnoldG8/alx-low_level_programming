be356d0fed05be6b1cb5b038882779b29534bcf0
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-19 13:37:22 -0700

    98 Battery st.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * main - Entry point
  * mul - function for multiplication
- *
  * @a: input
  * @b: input
  *
  * Return: Always 0 (Success)
  */
+int mul(int a, int b);
+int main(void)
+{
+	int x = mul(1, 2);
+	
+	return (x);
+}
 int mul(int a, int b)
 {
-	int c = a * b;
-
+	int c =  a * b;
 	return (c);
 }
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - function to reset value to 98
+ *
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
