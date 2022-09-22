07b54f3a19d75f5b801e17b3c37be577f504be89
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-22 03:05:58 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * _strcat - Concatenation function
  * main - main function
@@ -10,20 +11,9 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int count = 0;
+	char *strcat(char *dest, const char *src);
 
-	while (dest != '\0')
-	{
-		count++;
-	}
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[count] = src[i];
-		count += 1;
-	}
-	dest[count] = '\0';
-	return (dest);
+	return (*dest);
 }
 
 int main()
