120272fe060b519832f6131217f89c1b602281b7
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-23 01:29:20 -0700

    dan rewritten code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,30 @@
 #include "main.h"
-#include <string.h>
-/**_strncat(char *dest, char *src, int n)
- * @dest: input one
- * @src: input two
- * @n: input three
+
+/**
+ * _strncat - concatenate 2 string with certain of 
+ * @src: string concatenating 
+ * @dest: string being concatenated 
+ * @n: number of value 
+ *
+ * Return: string 
  */
-char *_strncat(char *dest, char *src, int n)
+
+char *_strncat(char * dest, char *src, int n)
 {
-	strncat(*dest, *src, n);
-	return (dest);
+	int dlen = 0, i = 0;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	while ( i < n && src[i])
+	{
+		dest[dlen] = src[i];
+		dlen++;
+		i++;
+	}
+	dest[dlen + n + 1] = '\0';
+
+	return dest(dest);
 }
