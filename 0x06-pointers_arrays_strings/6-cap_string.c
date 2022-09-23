2961e47eec74a7496287bc7e1a100c6770970d99
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-23 12:45:14 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,33 @@
 #include "main.h"
 /**
- * cap_string - make first letter capital in all words
- * @s: string to be changed
- * Return: cap string
+ * cap_string - captilize the first leter function
+ * @s: string input
+ *
+ * Return: New version of string
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	i = 0;
-
-	while (*(s + i) != '\0')
+	while (*(s + i))
 	{
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '\"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z')
-				*(s + i + 1) = *(s + i + 1) - 32;
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 'a' - 'A';
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 'a' - 'A';
+					}
+				}
+			}
 		}
 		i++;
 	}
