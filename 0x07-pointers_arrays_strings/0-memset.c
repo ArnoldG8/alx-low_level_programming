665635bbe8108cd075d99d9c346b26f02e77668f
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-26 09:22:52 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - function for replace data
+ *
+ * @s: string to be overwritten
+ * @b: byte to use for overwritting
+ * @n: number of bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j = 0;
+	while (j < n)
+	{
+		*(s + j) = b;
+		j++;
+	}
+	return (s);
+}
