535640054aab6a68724f614110baa9409a6de930
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-26 16:22:41 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - function for returning a pointer with the first occurence
+ * @s: first string to loop through
+ * @accept: second string to match vairables
+ * Return: s pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+					return (&s[i]);
+		}
+	}
+	return ('\0');
+}
