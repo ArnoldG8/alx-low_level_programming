a803e9ca1fce2a8714b4a77632516e8c30bf6537
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-26 09:48:25 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - function for copying
+ * @dest: first input
+ * @src: second input
+ * @n: third input
+ * Return: dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j = 0;
+
+	while (j <= n)
+	{
+		*(dest + j) = *(src + j);
+		j++;
+	}
+
+	return (dest);
+}
