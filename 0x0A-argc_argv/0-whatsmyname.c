19794539ca83b15d11922455336d444e7220e4c7
ArnoldG8 <arnoldaijuka@gmail.com>
2022-09-30 13:37:55 -0700

    Task 0, It aint what they call you, its what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of this file
+ * @argc: counts number fo commands entered
+ * @argv: strings stored of each command
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
