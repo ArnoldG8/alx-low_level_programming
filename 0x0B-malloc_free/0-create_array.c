cc2d9b9e06c3e5d3a9f293b2d1c791bbf9cce09c
ArnoldG8 <arnoldaijuka@gmail.com>
2022-10-04 12:18:25 -0700

    Task 0, Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - function that creates an array and puts a char
+ * @size: The size of the array
+ * @c: Array itslef
+ * Return: Always 0 (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	s = malloc(size * sizeof(char));
+
+	if (size <= 0)
+	{
+		return ('\0');
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(s + i) = c;
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the first look at Dynamic memory allocation.
